55986f6001a44e3aedc98c9f0d436156cb2de72c
Martin Souza <martinsoufer@gmail.com>
2022-07-06 05:50:25 -0300

    Typing

--- src/game.ts
+++ src/game.ts
@@ -1,51 +1,64 @@
-/// --- Set up a system ---
 
-class RotatorSystem {
-  // this group will contain every entity that has a Transform component
-  group = engine.getComponentGroup(Transform)
+interface Cell {
+  position: Vector3;
+  scale: Vector3;
+  rotation: Quaternion,
+  type?: string;
+  neighbours?: Array<Cell>;
+}
 
-  update(dt: number) {
-    // iterate over the entities of the group
-    for (const entity of this.group.entities) {
-      // get the Transform component of the entity
-      const transform = entity.getComponent(Transform)
+interface Tree {
+  genesis: Cell,
+  totalCells: number
+}
 
-      // mutate the rotation
-      transform.rotate(Vector3.Up(), dt * 10)
+const genesisCell: Cell = {
+  position: new Vector3(5, 0, 5),
+  rotation: new Quaternion(0, 0, 0, 0),
+  scale: new Vector3(0.1, 0.1, 0.1),
+  neighbours: []
+}
+
+
+let tree: Tree = {
+  genesis: genesisCell,
+  totalCells: 1
+}
+
+const updateTree = ():void => {
+
+}
+
+
+export class AnimationLoop implements ISystem {
+  updateTree();
+
+  update() {
+
+    const treeArr = Object.keys(tree)
+      .map((key: string) => {
+
+        return tree[key];
+      });
+
+    for (let cell of treeArr) {
+
+      const myEntity = new Entity()
+
+      const sphere = new SphereShape()
+      
+      myEntity.addComponent(
+        new Transform({
+          position: cell.position,
+          rotation: cell.rotation,
+          scale: cell.scale
+        })
+      )
+
+    //  engine.addEntity(myEntity)
     }
   }
 }
 
-// Add a new instance of the system to the engine
-engine.addSystem(new RotatorSystem())
 
-/// --- Spawner function ---
-
-function spawnCube(x: number, y: number, z: number) {
-  // create the entity
-  const cube = new Entity()
-
-  // add a transform to the entity
-  cube.addComponent(new Transform({ position: new Vector3(x, y, z) }))
-
-  // add a shape to the entity
-  cube.addComponent(new BoxShape())
-
-  // add the entity to the engine
-  engine.addEntity(cube)
-
-  return cube
-}
-
-/// --- Spawn a cube ---
-
-const cube = spawnCube(8, 1, 8)
-
-cube.addComponent(
-  new OnPointerDown(() => {
-    cube.getComponent(Transform).scale.z *= 1.1
-    cube.getComponent(Transform).scale.x *= 0.9
-
-    spawnCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
-  })
-)
+engine.addSystem(new AnimationLoop())
